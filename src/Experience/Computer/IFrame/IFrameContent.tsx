bb5d8f7c922b4753980c5a29ef4e798681d57bd4
yusif <yusifakhayev@gmail.com>
2022-12-08 08:33:48 -0700

    no idea

--- src/Experience/Computer/IFrame/IFrameContent.tsx
+++ src/Experience/Computer/IFrame/IFrameContent.tsx
@@ -1,76 +1,129 @@
 import {useEffect, useState} from 'react'
+import {a, useSpring} from 'react-spring'
 import {ComputerState} from '../../Store/ComputerState'
+
+import CSS from 'csstype'
 
 export const IFrameContent = () => {
     const toggleComputerState = ComputerState((state) => state.toggleComputerState)
+    const [booped, boop] = useState(false)
+    const timing = 100
+
+    const aButtonStyle = useSpring({
+        padding: '4rem',
+        paddingLeft: '0.5rem',
+        paddingRight: '0.5rem',
+        margin: '1.5rem',
+        alignItems: 'center',
+        fontSize: '2.25rem',
+        lineHeight: '1.75rem',
+        fontWeight: '700',
+        borderRadius: '0.25rem',
+        borderColor: 'white',
+        borderWidth: '1px',
+        boxSizing: 'border-box',
+        width: '33.333333%',
+        height: '50%',
+        color: booped
+            ? '#ff0000'
+            : '#475569'
+    })
 
     useEffect(() => {
-    },[])
+        if (!booped) return
+
+        const timeout = window.setTimeout(() => {
+            boop(booped => !booped)
+        }, timing)
+
+        return () => {
+            window.clearTimeout(timeout)
+        }
+    },[booped, timing])
+
+    const trigger = () => {
+        boop(true)
+    }
+
+    const innerWrapperStyle: CSS.Properties = {
+        flexDirection: 'column',
+        textAlign: 'center',
+        marginTop: '1.25rem',
+        marginBottom: '1.25rem'
+    }
+    const titleStyle: CSS.Properties =  {
+        fontSize: '3.25rem',
+        fontStyle: 'italic',
+        fontWeight: '800',
+        textAlign: 'center'
+    }
+
+    const wrapperDivStyle: CSS.Properties = {
+        display: 'flex',
+        flexDirection: 'column',
+        padding: '0.75rem',
+        fontFamily: 'Iosevka'
+    }
+
+    const buttonWrapperStyle: CSS.Properties = {
+        flexDirection: 'row',
+        textAlign: 'center'
+    }
+
+    const buttonStyle: CSS.Properties = {
+        padding: '4rem',
+        paddingLeft: '0.5rem',
+        paddingRight: '0.5rem',
+        margin: '1.5rem',
+        alignItems: 'center',
+        fontSize: '2.25rem',
+        lineHeight: '1.75rem',
+        fontWeight: '700',
+        color: 'rgb(71, 85, 105)',
+        borderRadius: '0.25rem',
+        borderColor: 'white',
+        borderWidth: '1px',
+        boxSizing: 'border-box',
+        width: '33.333333%',
+        height: '50%',
+    }
+
+    const aRefStyle: CSS.Properties = {
+        color: 'blue',
+        textDecoration: 'none',
+    }
 
     return (
         <div
-            style={{
-                display: 'flex',
-                flexDirection: 'column',
-                padding: '0.75rem',
-                fontFamily: 'Iosevka'
-            }}
+            style={wrapperDivStyle}
         >
-            <h1
-                style={{
-                    fontSize: '3.25rem', fontStyle: 'italic', fontWeight: '800',
-                    textAlign: 'center'
-                }}
-            >Welcome to <span style={{
-                color: 'blue'
-            }}>Shader World</span></h1>
+            {/* @ts-ignore */}
+            <h1 style={titleStyle} >Welcome to <a href='/' style={aRefStyle}>Shader World</a></h1>
 
-            <div
-                style={{
-                    flexDirection: 'column', textAlign: 'center', marginTop: '1.25rem', marginBottom: '1.25rem'
-                }}
-            >
-                <div
-                    style={{
-                        flexDirection: 'row', textAlign: 'center'
-                    }}
-                >
+            <div style={innerWrapperStyle} >
+                <div style={buttonWrapperStyle} >
                     <button
-                        style={{
-                            padding: '4rem', paddingLeft: '0.5rem', paddingRight: '0.5rem', margin: '1.5rem', alignItems: 'center', fontSize: '2.25rem', lineHeight: '1.75rem', fontWeight: '700', color: 'rgb(71, 85, 105)', borderRadius: '0.25rem', borderColor: 'white', borderWidth: '1px', boxSizing: 'border-box',
-                            width: '33.333333%', height: '50%',
-                        }}
+                        style={buttonStyle}
                         onClick={() => {
                             toggleComputerState('Sphere')
                         }}
                     >Sphere</button>
                     <button
-                        style={{
-                            padding: '4rem', paddingLeft: '0.5rem', paddingRight: '0.5rem', margin: '1.5rem', alignItems: 'center', fontSize: '2.25rem', lineHeight: '1.75rem', fontWeight: '700', color: 'rgb(71, 85, 105)', borderRadius: '0.25rem', borderColor: 'white', borderWidth: '1px', boxSizing: 'border-box',
-                            width: '33.333333%', height: '50%'
-                        }}
+                        style={buttonStyle}
                         onClick={() => {
                             toggleComputerState('Physics')
                         }}
                     >Physics</button>
 
                 </div>
-                <div
-                    style={{
-                        flexDirection: 'row', textAlign: 'center'
-                    }}
-                >
+                <div style={buttonWrapperStyle} >
+                    <a.button
+                        // @ts-ignore
+                        style={aButtonStyle}
+                        onMouseEnter={trigger}
+                    >Clouds</a.button>
                     <button
-                        style={{
-                            padding: '4rem', paddingLeft: '0.5rem', paddingRight: '0.5rem', margin: '1.5rem', alignItems: 'center', fontSize: '2.25rem', lineHeight: '1.75rem', fontWeight: '700', color: 'rgb(71, 85, 105)', borderRadius: '0.25rem', borderColor: 'white', borderWidth: '1px', boxSizing: 'border-box',
-                            width: '33.333333%', height: '50%'
-                        }}
-                    >Clouds</button>
-                    <button
-                        style={{
-                            padding: '4rem', paddingLeft: '0.5rem', paddingRight: '0.5rem', margin: '1.5rem', alignItems: 'center', fontSize: '2.25rem', lineHeight: '1.75rem', fontWeight: '700', color: 'rgb(71, 85, 105)', borderRadius: '0.25rem', borderColor: 'white', borderWidth: '1px', boxSizing: 'border-box',
-                            width: '33.333333%', height: '50%'
-                        }}
+                        style={buttonStyle}
                         onClick={() => {
                             toggleComputerState('Process')
                         }}
